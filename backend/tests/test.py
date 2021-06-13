d9f5eb5918b1096cf67404f8b1e1c635d9fe2283
a-ma-n <amanali.siddiqui2020@vitstudent.ac.in>
2021-06-14 02:26:34 +0530

    added with final changes

---
+++ backend/tests/test.py
@@ -0,0 +1,10 @@
+from helpers.search import *
+
+url = 'https://www.youtube.com/watch?v=r00ikilDxW4'
+
+# Calls the link function when -> link is entered
+# link(url)
+# Calls the keyword function when -> keyword is entered
+
+keyword_s('green day')
+
