a74e0a3831a645728d0878ed90d194add5fd7387
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2021-07-08 16:40:04 +0530

    Update search.py

--- Backend/helpers/search.py
+++ Backend/helpers/search.py
@@ -14,10 +14,8 @@ def searcher(search):
     * search-search is the keyword entered by the user
     * function returns a list of the comments of 15 videos in the form of a list
     """
-    # try :
     response = youtube.search().list(part='snippet', q=search, maxResults=15, type='video').execute()
-    # except:
-    #     print('comments were turned off for this video')
+    
     l = []
     for i in response['items']:
         l.append(i['id']['videoId'])
@@ -28,7 +26,6 @@ def keyword_s(search):
     a = []
     for i in searcher(search):
         if calc(i) != None:
-            #check if theres an item in the dtring => but we would ned to chk if comments are on
             a.append(calc(i))
     a = sorted(a, key=lambda i: i['rating'], reverse=True)
     print(a)
